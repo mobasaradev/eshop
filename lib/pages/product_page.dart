266ec7d36979ec95f7287b6224eff8705905c987
dev-Sara <mkmobasara.dev@gmail.com>
2022-12-16 01:28:21 +0600

    add widget search_bar in product_page

--- lib/pages/product_page.dart
+++ lib/pages/product_page.dart
@@ -1,3 +1,5 @@
+import 'package:e_shop/utils/utils.dart';
+import 'package:e_shop/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,6 +7,23 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: AppColors.primaryBackground,
+      appBar: AppBar(
+        backgroundColor: AppColors.transparent,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: const [
+            SearchBar(),
+            SizedBox(
+              height: 15,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
