7903156e770a4447db7dd685caa609e8579a7304
dev-Sara <mkmobasara.dev@gmail.com>
2022-12-16 01:18:37 +0600

    add utils app_colors

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+  static const primaryBackground = Color(0xFFF7F2FF);
+  static const secondaryColor = Color(0xffffffff);
+  static const searchIconColor = Color(0xFF6B6B6B);
+  static const buttonBarBackGround = Color(0xFFFFCCE4);
+  static const decrementButton = Color(0xFFFFBFDD);
+  static const transparent = Colors.transparent;
+  static const primaryText = Color(0xFFDA2079);
+  static const secondaryText = Color(0xFF323232);
+  static const secondaryText1 = Color(0xFF646464);
+  static final linearGradientAdd2Cart = [
+    const Color(0xFF6210E1).withOpacity(.5),
+    const Color(0xFF1400AE),
+  ];
+}
